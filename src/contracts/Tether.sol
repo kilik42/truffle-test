1329d3989c9683a0b98eeb6f1c7d884eb40cb746
kilik42 <maevins@cps.edu>
2022-12-31 00:12:27 -0600

    working tether contract

---
+++ src/contracts/Tether.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.5.0;
+
+contract Tether {
+    string public name = "Tether";
+    string public symbol = "USDT";
+    uint256 public decimals = 18;
+    uint256 public totalSupply = 1000000000000000000000000; // 1 million tokens
+    address public owner;
+
+    constructor() public {
+        owner = msg.sender;
+    }
+    modifier onlyOwner()  {
+        require(msg.sender == owner);
+        _;
+    }
+    mapping(address => uint256) public balanceOf;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    
+}
